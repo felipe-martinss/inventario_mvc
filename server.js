1b73885385a0284ed90f2ea6fb9a473d8ab432f9
Seu Nome <Seu E-mail>
2026-09-17 17:12:20 -0300

    up

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express")
 const cors = require("cors")
-
 const rotaInicial = (req, res) => {
     res.json("Back-end respondendo")
 }
+
+const routes = require("./src/routes")
 
 //Configurações do servidor
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 const porta = 3000
 
 //Rotas
 app.get('/', rotaInicial)
+app.use(routes)
 
 app.listen(porta, () => {
     console.log(`Servidor respondendo em: http://localhost:${porta}`)
